f2005ecaa6e768af5aee418fa53ff9fd61f3d808
Atharva Anand <atharva94311@gmail.com>
2025-12-07 14:59:01 +0530

    Update: made changes to my code

--- Assets/birdscript.cs
+++ Assets/birdscript.cs
@@ -4,12 +4,25 @@ public class birdscript : MonoBehaviour
 {
     public Rigidbody2D myrigidbody2D;
     public float flapstrength ;
+    public Logicscript logic;
+    public bool isalive=true;
+
+ void Start()
+    {
+        logic=GameObject.FindGameObjectWithTag("logic").GetComponent<Logicscript>();
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isalive)
         {
             myrigidbody2D.linearVelocity = Vector2.up * flapstrength;
         }
+       
     }
+     private void OnCollisionEnter2D(Collision2D collision)
+        {
+            logic.gameover();
+        isalive=false;
+        }
 }
--- Assets/logicscript.cs
+++ Assets/logicscript.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Logicscript : MonoBehaviour
 {
+    
     public int playerscore;
+    public GameObject gameoverscreen;
     public Text scoretext; 
     [ContextMenu("addscore")]
     public void addscore()
     {playerscore++;
        scoretext.text=playerscore.ToString();
        
+    }
+    public void restartgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void gameover()
+    {
+        gameoverscreen.SetActive(true);
     }
 }
